24cbb62800a1bf608f0d2353689eb4560a7419f5
nikhil-muralidharan <36506837+nikhil-muralidharan@users.noreply.github.com>
2020-03-21 13:42:02 +0530

    Add files via upload

--- registartion_ver_0_1.py
+++ registartion_ver_0_1.py
@@ -5,11 +5,27 @@
 # Created by: PyQt5 UI code generator 5.13.2
 #
 # WARNING! All changes made in this file will be lost!
-
-
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import base64
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import mimetypes
+import os
+from apiclient import errors
+import math
+import random 
+import send_message
 from PyQt5 import QtCore, QtGui, QtWidgets
-
-
+otp = 0
+nid_db = {'2':{'email' : 'nikhilmuralidharan98@gmail.com' , 'wallet':'0x667'}}
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
@@ -90,6 +106,10 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        #clicks
+        self.verify_button.clicked.connect(self.clicked_verify)
+        self.otp_button.clicked.connect(self.clicked_generate)
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
@@ -102,6 +122,53 @@ class Ui_MainWindow(object):
         self.NID.setText(_translate("MainWindow", "Enter NID Number : "))
         self.wallet_label.setText(_translate("MainWindow", "Enter Wallet ID : "))
 
+    def clicked_generate(self):
+        value1 = self.nid_ip.toPlainText()
+        value1 = value1.strip()
+        value2 = self.email_ip.toPlainText()
+        value2 = value2.strip()
+        value3 = self.wallet_id_ip.toPlainText()
+        value3 = value3.strip()
+        if value1 in nid_db:
+            email = nid_db[value1]['email']
+            walletid = nid_db[value1]['wallet']
+            if email == value2 and walletid ==value3:
+                otp = send_message.otp_gen()
+                creds = None
+                if os.path.exists('token.pickle'):
+                    with open('token.pickle', 'rb') as token:
+                        creds = pickle.load(token)
+                # If there are no (valid) credentials available, let the user log in.
+                if not creds or not creds.valid:
+                    if creds and creds.expired and creds.refresh_token:
+                        creds.refresh(Request())
+                    else:
+                        flow = InstalledAppFlow.from_client_secrets_file(
+                            'credentials.json', SCOPES)
+                        creds = flow.run_local_server(port=0)
+                        # Save the credentials for the next run
+                    with open('token.pickle', 'wb') as token:
+                        pickle.dump(creds, token)
+                #print(otp)
+                otp_message = 'Please Enter The One Time Password(OTP) On The Registartion Tool.\n'+'Your OTP is : '+otp
+                service = build('gmail', 'v1', credentials=creds)
+                message = send_message.create_msg('gameboy9112012@gmail.com',email,'DPKI One Time Password (OTP)',otp_message)
+                x = send_message.send_msg(service,message)
+                self.res_label.setText("OTP Sent Successfully Please Verify")   
+                self.res_label.adjustSize()
+            else:
+                self.res_label.setText("Entered Email Or Wallet ID mismatch")   
+                self.res_label.adjustSize()
+        else:
+            self.res_label.setText("NID not in AADHAR Database")    
+            self.res_label.adjustSize()    
+
+    def clicked_verify(self):
+        #print('clicked')
+        #email_ip
+        textboxValue = self.email_ip.toPlainText()
+        print(textboxValue)
+        #self.label.adjustSize()    
 
 if __name__ == "__main__":
     import sys
